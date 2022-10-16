961fc4a34f11d81e8763a12b2982a0acdc671a7f
Bruno Campos Gomes <up201906401@fe.up.pt>
2022-10-16 22:03:43 +0000

    Fix/primitive warnings

--- tp1/src/parser/primitives.js
+++ tp1/src/parser/primitives.js
@@ -7,24 +7,30 @@ import { MyRectangle } from '../primitives/MyRectangle.js';
      */
 export function parsePrimitives(primitivesNode, graph) {
     const children = primitivesNode.children;
+    if (children.length == 0){
+        return 'No primitives defined'
+    }
+
     graph.primitives = [];
 
     // Any number of primitives.
     for (let i = 0; i < children.length; i++) {
 
         if (children[i].nodeName != "primitive") {
-            graph.onXMLMinorError("unknown tag <" + children[i].nodeName + ">");
+            graph.onXMLMinorError(`unknown tag <${children[i].nodeName}>`);
             continue;
         }
 
         // Get id of the current primitive.
         const primitiveId = graph.reader.getString(children[i], 'id');
-        if (primitiveId == null)
-            return "no ID defined for texture";
+        if (primitiveId == null || primitiveId == ''){
+            return "no ID defined for primitive in <primitives> block";
+        }
 
         // Checks for repeated IDs.
-        if (graph.primitives[primitiveId] != null)
-            return "ID must be unique for each primitive (conflict: ID = " + primitiveId + ")";
+        if (graph.primitives[primitiveId] != null){
+            return `ID must be unique for each primitive (conflict: ID = ${primitiveId})`;
+        }
         // Specifications for the current primitive.
         
         const primitive = graph.factory.build(children[i], graph.reader, graph.scene);
@@ -33,5 +39,5 @@ export function parsePrimitives(primitivesNode, graph) {
     }
 
     graph.log("Parsed primitives");
-    return null;
+    return;
 }
