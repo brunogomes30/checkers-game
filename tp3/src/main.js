02b7f9bb0188bfed80ce9e8e2d8a7f3bb63c6728
up201907216 <up201907216@edu.fe.up.pt>
2022-12-29 19:30:54 +0000

    Add first scene from url

--- tp3/src/main.js
+++ tp3/src/main.js
@@ -4,7 +4,21 @@ import { MyInterface } from './MyInterface.js';
 import { MySceneGraph } from './MySceneGraph.js';
 import { buildDebugFolder } from './interface/build.js';
 
- function main() {
+function getUrlVars() {
+    var vars = {};
+    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+        function (m, key, value) {
+            vars[decodeURIComponent(key)] = decodeURIComponent(value);
+        });
+    return vars;
+}
+
+
+function main() {
+    // get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml 
+    // or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor) 
+
+    const filename = getUrlVars()['file'] || 'demo.xml';
 
     // Standard application, scene and interface setup
     const app = new CGFapplication(document.body);
@@ -20,14 +34,17 @@ import { buildDebugFolder } from './interface/build.js';
     buildDebugFolder(myInterface, myScene);
 
     const sceneGraphs = [];
+    sceneGraphs[filename] = new MySceneGraph(filename, myScene);
     sceneGraphs['room'] = new MySceneGraph('demo.xml', myScene, 'room');
+    
+    
     sceneGraphs['blendertest'] = new MySceneGraph('blendertest.xml', myScene, 'blendertest');
 
 
     const currentGraph = { value: Object.keys(sceneGraphs)[0] };
     myScene.graph = sceneGraphs[currentGraph.value];
     myScene.graph.selected = true;
-    
+
     myInterface.gui.add(currentGraph, 'value', Object.keys(sceneGraphs)).name('Scene').onChange(() => {
         myScene.graph.ui.hide();
         myScene.graph = sceneGraphs[currentGraph.value];
